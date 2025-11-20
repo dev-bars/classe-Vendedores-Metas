67a22efdfc4346809120cf3e9900e8733adc33ec
renan-bars <156934542+dev-bars@users.noreply.github.com>
2025-11-20 20:02:18 -0300

    limpando o código e aplicando comentários
    Clean code é essencial, removi variáveis redundantes e deixei o código mais legível.
    Inseri comentários objetivos para compreensão do código por demais Devs.

--- mainVendedoresMetas.py
+++ mainVendedoresMetas.py
@@ -1,26 +1,27 @@
-class Vendedor():
-    def __init__(self, nome):
-        self.nome = nome
+#Criei a classe Vendedor, que contem os atributos necessários para o cadastro de um novo vendedor e do valor de suas vendas.
+#Esta classe recebe os atributos nome e vendas.
 
-    def vendasRealizadas(self, vendas):
-        self.vendasRealizadas = vendas
-    
+class Vendedor():
+    def __init__(self, nome, vendas):
+        self.nome = nome
+        self.vendas = vendas
+#Já o construtor, efetua uma função para identificar se os vendedores bateram a meta ou não.
+
     def metaDefinida(self, meta):
-        if self.vendasRealizadas >= meta:
+        if self.vendas >= meta:
             print(f"{self.nome} Bateu a meta")
         else:
-            print(f"{self.nome} Não bateu")
+            print(f"{self.nome} Não bateu a meta")
 
 #Variaveis criadas sendo cada um dos vendedores da loja            
-vendedor1 = Vendedor("Marcos")
-vendedor1.vendasRealizadas(1000)
+vendedor1 = Vendedor("Marcos", 1000)
 vendedor1.metaDefinida(500)
 
 
-vendedor2 = Vendedor("Beatriz")
-vendedor2.vendasRealizadas(400)
+vendedor2 = Vendedor("Beatriz", 300)
 vendedor2.metaDefinida(500)
 
+#Paraa saída de dados, são pelos dados indicados retornam o resultado do if/else conforme definição correspondente.
 
 #1° Membro - Atributos
 #2° Membro - Propriedades
